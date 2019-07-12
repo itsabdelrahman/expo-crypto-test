cf318216b88d594b553f1799dc4fc8a5dfff6278
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2019-07-12 23:24:58 +0200

    :hammer: Run tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { sha1, hexToASCII } from './utilities';
+import testVectors from './test-vectors';
 
-export default class App extends React.Component {
+type Props = {};
+type State = {};
+
+export default class App extends React.Component<Props, State> {
+  componentDidMount() {
+    testVectors.sha1.forEach(async (vector, index) => {
+      const { message, digest: expectedDigest } = vector;
+
+      const messageASCII = hexToASCII(message);
+      const computedDigest = await sha1(messageASCII);
+
+      if (computedDigest === expectedDigest) {
+        console.log(`${index}: ✅`);
+      } else {
+        console.log(
+          `${index}: ❌  [ Message -> ${message}: expected -> ${expectedDigest}, computed -> ${computedDigest} ]`,
+        );
+      }
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
