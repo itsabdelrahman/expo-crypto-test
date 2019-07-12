858408bb9c643aadb88e5fcc5076b640e6e46091
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2019-07-12 23:24:42 +0200

    :bento: Add SHA-1 test vectors

---
+++ src/test-vectors/index.ts
@@ -0,0 +1,44 @@
+export default {
+  sha1: [
+    {
+      message: '00',
+      digest: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+    },
+    {
+      message: '36',
+      digest: 'c1dfd96eea8cc2b62785275bca38ac261256e278',
+    },
+    {
+      message: '195a',
+      digest: '0a1c2d555bbe431ad6288af5a54f93e0449c9232',
+    },
+    {
+      message: 'df4bd2',
+      digest: 'bf36ed5d74727dfd5d7854ec6b1d49468d8ee8aa',
+    },
+    {
+      message: '549e959e',
+      digest: 'b78bae6d14338ffccfd5d5b5674a275f6ef9c717',
+    },
+    {
+      message: 'f7fb1be205',
+      digest: '60b7d5bb560a1acf6fa45721bd0abb419a841a89',
+    },
+    {
+      message: 'c0e5abeaea63',
+      digest: 'a6d338459780c08363090fd8fc7d28dc80e8e01f',
+    },
+    {
+      message: '63bfc1ed7f78ab',
+      digest: '860328d80509500c1783169ebf0ba0c4b94da5e5',
+    },
+    {
+      message: '7e3d7b3eada98866',
+      digest: '24a2c34b976305277ce58c2f42d5092031572520',
+    },
+    {
+      message: '9e61e55d9ed37b1c20',
+      digest: '411ccee1f6e3677df12698411eb09d3ff580af97',
+    },
+  ],
+};
